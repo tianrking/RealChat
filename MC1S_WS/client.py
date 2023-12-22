4de15a8b6fe571f15b608d558e4091c783316726
tianrking <tian.r.king@gmail.com>
2023-12-22 12:29:27 +0800

    Changes to be committed: 	modified:   MC1S_WS/client.py 	modified:   MC1S_WS/server.py 	modified:   MC1S_WS/web.html

--- MC1S_WS/client.py
+++ MC1S_WS/client.py
@@ -1,23 +1,29 @@
 import asyncio
 import websockets
+import json
+
+# 假设这是客户端的唯一标识，实际应用中可能是用户输入或随机生成
+client_id = "client1"
 
 async def receive_message(websocket):
     while True:
         message = await websocket.recv()
-        print(f"\rReceived: {message}\nYou: ", end='')
+        data = json.loads(message)
+        # 如果信息来源不是自己，则打印信息
+        if data["sender"] != client_id:
+            print(f"\rReceived from {data['sender']}: {data['message']}\nYou: ", end='')
 
 async def send_message(websocket):
     while True:
         message = await asyncio.get_event_loop().run_in_executor(None, input, "You: ")
-        await websocket.send(message)
+        # 发送带有发送者标识的消息
+        await websocket.send(json.dumps({"sender": client_id, "message": message}))
 
 async def main():
     uri = "ws://localhost:12345"
     async with websockets.connect(uri) as websocket:
-        # 同时启动发送和接收任务
         receive_task = asyncio.create_task(receive_message(websocket))
         send_task = asyncio.create_task(send_message(websocket))
-        # 等待任务完成
         done, pending = await asyncio.wait(
             [receive_task, send_task],
             return_when=asyncio.FIRST_COMPLETED,
--- MC1S_WS/server.py
+++ MC1S_WS/server.py
@@ -1,25 +1,28 @@
 import asyncio
 import websockets
+import json
 
 connected = set()
 
-async def echo(websocket, path):
-    print(f"A client has connected.")
+async def server(websocket, path):
+    global connected
     connected.add(websocket)
     try:
         async for message in websocket:
-            print(f"Received message from client: {message}")
-            for conn in connected:
-                await conn.send(message)
-    except websockets.exceptions.ConnectionClosed as e:
-        print(f"A client has disconnected.")
+            try:
+                data = json.loads(message)
+                print(f"Received message {data['message']} from {data['sender']}")
+                new_message = json.dumps({"sender": data['sender'], "message": data['message']})
+                for conn in connected:
+                    if conn != websocket:  # Do not send the message back to the sender
+                        await conn.send(new_message)
+            except json.JSONDecodeError:
+                print("Received an invalid message.")
     finally:
         connected.remove(websocket)
 
-async def main():
-    async with websockets.serve(echo, "localhost", 12345):
-        await asyncio.Future()  # run forever
+start_server = websockets.serve(server, "localhost", 12345)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
 
